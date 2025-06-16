732c65d75de32210da951fd760ed954bab2623cb
Luiz Henrique Souza Ventura <luiz.ventura01@etec.sp.gov.br>
2025-06-16 17:44:10 -0300

    commit_1

---
+++ calculadora.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int n1, n2, r;
+	char  op
+
+}
+
+void soma(){
+
+}
+
+void subtracao(){
+
+}
+
+void multi(){
+
+}
+
+void divisao(){
+
+}
